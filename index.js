71882db00054cd9f5d061606a282456fd04c043c
Ji Chen <jaykchen@gmail.com>
2023-06-20 21:44:18 -0400

    remove urls not visible by eval, page innerText

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 const puppeteer = require('puppeteer');
-const pdfParse = require('pdf-parse');
-const { extractFromHtml } = require('@extractus/article-extractor');
-const { htmlToText } = require('html-to-text');
 
 async function text_from_ensemble_methods(url) {
     console.log(`Starting the browser and going to ${url}...`);
@@ -9,7 +6,7 @@ async function text_from_ensemble_methods(url) {
     const browser = await puppeteer.launch({
         headless: true,
         defaultViewport: { width: 820, height: 1180 },
-        // executablePath: '/path/to/your/chrome',
+        executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
     });
 
     const page = await browser.newPage();
@@ -17,49 +14,14 @@ async function text_from_ensemble_methods(url) {
     await page.goto(url, { waitUntil: 'networkidle0' });
     await page.waitForSelector('body', { timeout: 10000 });
 
-    const page_html_as_str = await page.content();
-    const page_html_cleaned_obj = await extractFromHtml(page_html_as_str, url);
-    const readability_text = htmlToText(page_html_cleaned_obj.content, {
-        wordwrap: 80,
-        hideLinkHrefIfSameAsText: true,
-    });
+    const textContent = await page.evaluate(() => document.body.innerText);
 
-    const pdfOptions = {
-        format: 'Letter',
-        width: '11in',
-        height: '17in',
-        landscape: false,
-        displayHeaderFooter: false,
-        printBackground: false,
-        margin: {
-            top: "0.1in",
-            right: "0.1in",
-            bottom: "0.1in",
-            left: "0.1in"
-        },
-        preferCSSPageSize: false,
-    };
-
-    const pdf_buffer = await page.pdf(pdfOptions);
-
-    const data = await pdfParse(pdf_buffer);
-    const text_from_page_virtual_pdf = data.text;
-
-    if (readability_text.length > 400 && readability_text.length < text_from_page_virtual_pdf.length) {
-        console.log(`Text extracted with Readability:`);
-        console.log(readability_text);
-
-        return readability_text;
-    } else {
-        console.log(`Text extracted from the PDF:`);
-        console.log(data.text);
-        return data.text;
-    }
+    console.log(textContent);
     await browser.close();
 }
 
-// text_from_ensemble_methods('https://www.npmjs.com/package/html-to-text')
-text_from_ensemble_methods('https://github.com/amiiiiii830/')
+// text_from_ensemble_methods('https://github.com/amiiiiii830/')
+text_from_ensemble_methods('https://nature.com/articles/d41586-023-01980-4')
     .catch(err => {
         console.error(`An error occurred: ${err}`);
     });
