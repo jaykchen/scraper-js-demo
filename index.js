e462ca9464f2beeddbf77feb22cea84be803f617
Ji Chen <jaykchen@gmail.com>
2023-07-30 21:31:31 -0400

    load credentials from file

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import express from 'express';
 import puppeteer from 'puppeteer';
-import { extract } from '@extractus/article-extractor'
+import { extractFromHtml } from '@extractus/article-extractor'
+import { JSDOM } from 'jsdom';
+import fs from 'fs';
+
+// Load environment variables from env.json file
+const env = JSON.parse(fs.readFileSync('env.json', 'utf-8'));
 
 const app = express();
 const port = 3000;
+
+// Parse the LOGIN_CREDENTIALS from env.json into an object
+const loginCredentials = env.LOGIN_CREDENTIALS || [];
 
 app.get('/', async (req, res) => {
     const url = req.query.url;
@@ -12,26 +20,49 @@ app.get('/', async (req, res) => {
         return res.status(400).send('Invalid url');
     }
 
+    let pageHTML;
+    let textContent;
+
+    // Find matching credentials
+    const creds = loginCredentials.find(creds => url.includes(creds.url));
+
+    if (creds) {
+        // Login using credentials if they exist
+        try {
+            const browser = await puppeteer.launch({
+                headless: true,
+                defaultViewport: { width: 820, height: 1180 },
+                userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+                executablePath: '/Applications/Google Chrome Beta.app/Contents/MacOS/Google Chrome Beta',
+            });
+            const page = await browser.newPage();
+            await page.goto(`https://${creds.url}/login`, { waitUntil: 'networkidle0' });
+            await page.type('#login_field', creds.username); // Enter your username
+            await page.type('#password', creds.password); // Enter your password from env.json
+            await Promise.all([
+                page.waitForNavigation(), // The promise resolves after navigation has finished
+                page.click('.btn-primary'), // Clicking the link will indirectly cause a navigation
+            ]);
+            pageHTML = await fetchPageHTML(url, page);
+            textContent = await page.evaluate(() => document.body.innerText);
+        } catch (err) {
+            console.error('Login failed:', err);
+        }
+    }
+
     let articleExtractorText = null;
     try {
-        articleExtractorText = await extractArticle(url);
-        // console.log(articleExtractorText.content);
+        const dom = new JSDOM(pageHTML);
+        articleExtractorText = await extractArticle(url, dom.window.document);
     } catch (err) {
         console.error("Error in extractArticle: ", err.message);
     }
 
-    let chromeExtractedText = null;
-    try {
-        chromeExtractedText = await chromeExtract(url);
-    } catch (err) {
-        console.error("Error in chromeExtract: ", err.message);
-    }
-
     if (articleExtractorText && articleExtractorText.content.length > 100 &&
-        chromeExtractedText && articleExtractorText.content.length < chromeExtractedText.length) {
+        textContent && articleExtractorText.content.length < textContent.length) {
         return res.status(200).send(articleExtractorText.content);
-    } else if (chromeExtractedText) {
-        return res.status(200).send(chromeExtractedText);
+    } else if (textContent) {
+        return res.status(200).send(textContent);
     } else {
         return res.status(500).send('Both extractors failed.');
     }
@@ -41,9 +72,9 @@ app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
 });
 
-async function extractArticle(url) {
+async function extractArticle(url, doc) {
     try {
-        const data = await extract(url);
+        const data = await extractFromHtml(doc, url);
         return data;
     } catch (err) {
         console.error(err.message);
@@ -51,23 +82,7 @@ async function extractArticle(url) {
     }
 }
 
-async function chromeExtract(url) {
-    const browser = await puppeteer.launch({
-        headless: true,
-        // userDataDir: '/Users/jaykchen/Library/Application Support/Google/Chrome/Default',
-        // userDataDir: '/Users/jaykchen/Library/Application Support/Google/Chrome Beta/Default',
-        userDataDir: '/Users/jaykchen/Library/Application Support/Google/Chrome Beta/',
-        args: ['--no-sandbox', '--disable-setuid-sandbox',
-            '--profile-directory=/Users/jaykchen/Library/Application Support/Google/Chrome Beta/Profile 1'],
-        // args: ['--no-sandbox', '--disable-setuid-sandbox',
-        //     '--profile-directory=/Users/jaykchen/Library/Application Support/Google/Chrome Beta/Profile 1'],
-        // userDataDir: '/Users/jaykchen/Library/Application Support/Google/Chrome/jaykchen@gmail.com',
-        defaultViewport: { width: 820, height: 1180 },
-        executablePath: '/Applications/Google Chrome Beta.app/Contents/MacOS/Google Chrome Beta',
-        // executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-    });
-    const page = await browser.newPage();
-
+async function fetchPageHTML(url, page) {
     try {
         const response = await page.goto(url, { waitUntil: 'networkidle0' });
         await page.waitForSelector('body', { timeout: 10000 });
@@ -82,10 +97,8 @@ async function chromeExtract(url) {
         }
 
         await page.waitForSelector('body', { timeout: 5000 });
-        const textContent = await page.evaluate(() => document.body.innerText);
-        console.log(textContent);
-        return textContent;
-
+        const htmlContent = await page.evaluate(() => document.documentElement.outerHTML);
+        return htmlContent;
     } catch (e) {
         console.error(e.toString());
         return "";
@@ -94,3 +107,17 @@ async function chromeExtract(url) {
         // await browser.close();
     }
 }
+
+
+// userDataDir: '/Users/jaykchen/Library/Application Support/Google/Chrome/Default',
+// userDataDir: '/Users/jaykchen/Library/Application Support/Google/Chrome Beta/Default',
+// userDataDir: '/Users/jaykchen/Library/Application Support/Google/Chrome Beta/',
+// args: ['--no-sandbox', '--disable-setuid-sandbox',
+//     '--profile-directory=/Users/jaykchen/Library/Application Support/Google/Chrome Beta/Profile 1'],
+// args: ['--no-sandbox', '--disable-setuid-sandbox',
+//     '--profile-directory=/Users/jaykchen/Library/Application Support/Google/Chrome Beta/Profile 1'],
+// userDataDir: '/Users/jaykchen/Library/Application Support/Google/Chrome/jaykchen@gmail.com',
+// defaultViewport: { width: 820, height: 1180 },
+// userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+//     executablePath: '/Applications/Google Chrome Beta.app/Contents/MacOS/Google Chrome Beta',
+//         // executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
